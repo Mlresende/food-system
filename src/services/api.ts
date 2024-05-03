a517911ce4fbcd35928f1ed473963f71f53c9d71
mlresende <mlr130203.br@gmail.com>
2024-05-03 02:51:24 -0300

    Novo link api

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,15 +2,15 @@ import axios from 'axios'
 import { SnackData } from '../interfaces/SnackData'
 
 // Defina o URL base padrão aqui
-const defaultBaseUrl = 'https://food-system-cmcw0vv4i-mlresendes-projects.vercel.app/'
+const defaultBaseUrl = 'https://food-system-db.vercel.app/'
 
 const api = axios.create({
   baseURL: defaultBaseUrl,
 })
 
-export const getBurgers = () => api.get<SnackData[]>('https://food-system-cmcw0vv4i-mlresendes-projects.vercel.app/burgers')
-export const getPizzas = () => api.get<SnackData[]>('food-system-cmcw0vv4i-mlresendes-projects.vercel.app/pizzas')
-export const getDrinks = () => api.get<SnackData[]>('food-system-cmcw0vv4i-mlresendes-projects.vercel.app/drinks')
-export const getIceCreams = () => api.get<SnackData[]>('food-system-cmcw0vv4i-mlresendes-projects.vercel.app/ice-creams')
+export const getBurgers = () => api.get<SnackData[]>('/burgers')
+export const getPizzas = () => api.get<SnackData[]>('/pizzas')
+export const getDrinks = () => api.get<SnackData[]>('/drinks')
+export const getIceCreams = () => api.get<SnackData[]>('/ice-creams')
 
 export default api
