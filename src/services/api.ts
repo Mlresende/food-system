01f99a0ebc7cc5f788d271fb2f5b84adb883b09b
mlresende <mlr130203.br@gmail.com>
2024-05-03 02:48:25 -0300

    a

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,9 +8,9 @@ const api = axios.create({
   baseURL: defaultBaseUrl,
 })
 
-export const getBurgers = () => api.get<SnackData[]>('/burgers')
-export const getPizzas = () => api.get<SnackData[]>('/pizzas')
-export const getDrinks = () => api.get<SnackData[]>('/drinks')
-export const getIceCreams = () => api.get<SnackData[]>('/ice-creams')
+export const getBurgers = () => api.get<SnackData[]>('https://food-system-cmcw0vv4i-mlresendes-projects.vercel.app/burgers')
+export const getPizzas = () => api.get<SnackData[]>('food-system-cmcw0vv4i-mlresendes-projects.vercel.app/pizzas')
+export const getDrinks = () => api.get<SnackData[]>('food-system-cmcw0vv4i-mlresendes-projects.vercel.app/drinks')
+export const getIceCreams = () => api.get<SnackData[]>('food-system-cmcw0vv4i-mlresendes-projects.vercel.app/ice-creams')
 
 export default api
